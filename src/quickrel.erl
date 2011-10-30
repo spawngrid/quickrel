ead7c9c7d7bc3fbc126044b88ad38ac842b8c6d8
Yurii Rashkovskii <yrashk@gmail.com>
2011-10-30 10:01:29 -0700

    Create start script

--- src/quickrel.erl
+++ src/quickrel.erl
@@ -1,5 +1,6 @@
 -module(quickrel).
 -export([build/2]).
+-include_lib("kernel/include/file.hrl").
 
 build(Reltool, Path0) ->
    Path = filename:absname(Path0),
@@ -23,6 +24,23 @@ build(Reltool, Path0) ->
    file:write_file(ScriptFile, io_lib:format("~p.~n",[Script])),
    systools:script2boot(filename:join([filename:dirname(ScriptFile),
          filename:basename(ScriptFile, ".script")])),
+
+   {ok, [[RootDir]]} = init:get_argument(root),
+   ErtsVersion = erlang:system_info(version),
+   file:write_file(filename:join([Path, "start"]),
+                   io_lib:format(
+                   "#! /bin/sh\n"
+                   "ERTS_DIR=~s\n"
+                   "ROOTDIR=$(cd ${0%/*} && pwd)\n"
+                   "EMU=beam\n"
+                   "BINDIR=$ERTS_DIR/bin\n"
+                   "cd $ROOTDIR\n"
+                   "export ROOTDIR EMU BINDIR\n"
+                   "exec $BINDIR/erlexec -boot ~s $@\n",
+                   [filename:join([RootDir, "erts-" ++ ErtsVersion]), 
+                    filename:join([Path, BootRel])])),
+   {ok, FI} = file:read_file_info(filename:join([Path, "start"])),
+   file:write_file_info(filename:join([Path, "start"]), FI#file_info{ mode = FI#file_info.mode bor 8#00110 }),  
 
    %% 
    ok.
